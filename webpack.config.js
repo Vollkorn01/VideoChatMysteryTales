b6124673426866253946e25f0156a27a89f84a9f
Benjamin Gallusser <bgallusser@googlemail.com>
2020-03-29 23:21:26 +0200

    finish vuetify setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,21 @@
 module.exports = {
   rules: [
     {
+      test: /\.css$/,
+      use: [
+        'vue-style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            // enable CSS Modules
+            modules: true,
+            // customize generated class names
+            localIdentName: '[local]_[hash:base64:8]'
+          }
+        }
+      ]
+    },
+    {
       test: /\.s(c|a)ss$/,
       use: [
         'vue-style-loader',
