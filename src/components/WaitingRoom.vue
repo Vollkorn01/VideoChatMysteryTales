19afa06f518adf04648f12aaca597aebe0f0c89b
Alexander Pfyffer <alexander.pfyffer@gmail.com>
2020-03-29 21:57:40 +0200

    creating session

--- src/components/WaitingRoom.vue
+++ src/components/WaitingRoom.vue
@@ -1,18 +1,68 @@
 <template>
   <div>
-      WaitingRoom
+    <div>Waiting Room</div>
+    <div>Code:</div>
+    <div>{{code}}</div>
+    <div>Send this code to your friends, so they can join!</div>
   </div>
 </template>
 <script>
+import { db } from "../firebase";
+
 export default {
-  name: 'WaitingRoom',
-  components: {
+  name: "WaitingRoom",
+  components: {},
+  data() {
+    return {
+      games: [],
+      code: "",
+      gameOne: {}
+    };
+  },
+  firestore: {
+    games: db.collection("games"),
+    gameOne: db.collection("games").doc("8eouFSKemqC5niZiGu2U")
+  },
+  computed: {},
+  methods: {
+    /**
+     * generates a random code
+     */
+    generateCode() {
+      const length = 5;
+      let result = "";
+      const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      const charactersLength = characters.length;
+      for (var i = 0; i < length; i++) {
+        result += characters.charAt(
+          Math.floor(Math.random() * charactersLength)
+        );
+      }
+      this.code = result;
+    },
+
+    /**
+     * creates a new game session on firestore and sets the code
+     */
+    createSessionFirestore(code) {
+      let data = {
+        code,
+        gameID: "",
+        playerIds: [2134, 1234, 1234],
+        zoomIds: [1234, 1234, 1234]
+      };
+      let createSession = db
+        .collection("sessions")
+        .doc(code)
+        .set(data);
+    }
   },
   created() {
-      console.log('user: ', this.$store.getters.user);
+    this.generateCode();
+    this.createSessionFirestore(this.code);
   }
 };
-
 </script>
 
 <style>
