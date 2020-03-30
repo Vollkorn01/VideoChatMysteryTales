bac03bca7096a6be76f6e425bda03b9b17efc2d8
Benjamin Gallusser <bgallusser@googlemail.com>
2020-03-30 14:47:59 +0200

    Add room switching feature

--- src/components/GameCharacters.vue
+++ src/components/GameCharacters.vue
@@ -1,12 +1,11 @@
 <template>
-<div class="container">
-  <h1>Characters</h1>
+  <div class="container">
+    <h1>Characters</h1>
     <v-expansion-panels multiple popout hover>
-      <v-expansion-panel
-        v-for="character in characters"
-        :key="character.id"
-      >
-        <v-expansion-panel-header>{{ character.name }}</v-expansion-panel-header>
+      <v-expansion-panel v-for="character in characters" :key="character.id">
+        <v-expansion-panel-header>{{
+          character.name
+        }}</v-expansion-panel-header>
         <v-expansion-panel-content>
           {{ character.publicDescription }}
         </v-expansion-panel-content>
@@ -24,19 +23,19 @@ export default {
       characters: {
         qegsdvb: {
           name: "Sepperl",
-          publicDescription: "Sepperl lives in the mountains",
+          publicDescription: "Sepperl lives in the mountains"
         },
         nvnklv: {
           name: "Hannes",
-          publicDescription: "Hannes lives by the river",
+          publicDescription: "Hannes lives by the river"
         },
         nvadsf: {
           name: "Rosina",
-          publicDescription: "Rosina lives in a castle",
-        },
-      },
+          publicDescription: "Rosina lives in a castle"
+        }
+      }
     };
-  },
+  }
   // name: "GameCharacters",
   //     firestore: {
   //       //   TODO
--- src/components/GameMap.vue
+++ src/components/GameMap.vue
@@ -9,9 +9,9 @@
         </v-toolbar>
 
         <v-list two-line subheader>
-          <v-list-item v-for="room in chatrooms" :key="room.id" @click="">
+          <v-list-item v-for="room in chatrooms" :key="room.name" @click="">
             <v-list-item-content>
-              <v-list-item-title>{{ room.id }}</v-list-item-title>
+              <v-list-item-title>{{ room.name }}</v-list-item-title>
 
               <v-list-item-subtitle>
                 {{ room.players }}
@@ -31,8 +31,8 @@
 
             <v-list-item-action>
               <!-- TODO change color of button or entire list item when you are in a room -->
-              <v-btn ripple>
-                <a :href="'https://zoom.us/j/' + room.zoomId" target="_blank">
+              <v-btn ripple @click="enterChatroom(room.id)">
+                <a :href="'https://zoom.us/j/' + room.zoom_id" target="_blank">
                   Join
                 </a>
               </v-btn>
@@ -45,63 +45,123 @@
 </template>
 
 <script>
-// import { db } from "../firebase";
+import { db } from "../firebase";
+// import store from "../store";
+import { mapGetters } from "vuex";
 
 export default {
+  name: "GameMap",
   components: {},
   data() {
     return {
-      session: {
-        "07mdu": {
-          code: "07mdu",
-          gameID: "",
-          playerIds: [2134, 1234, 1234],
-          zoomIds: [1234, 1234, 1234]
-        }
-      },
-      players: [
-        {
-          dummyid: {
-            character_id: "adffdcx",
-            chatroom_id: 1234,
-            user_uid: "B4Yo8z9ppGcNu4vNQYQOjkvLEz53",
-            vote_id: ""
-          }
-        }
-      ],
-      chatrooms: {
-        adsfadfdv: {
-          id: "Living room",
-          players: ["Sepp", "Hansi"],
-          zoomId: 842749965
-        },
-        dfsdfga: {
-          id: "Kitchen",
-          players: ["Michi", "Anabel"],
-          zoomId: 598515719
-        },
-        fhjkfj: {
-          id: "Rooftop",
-          players: [],
-          zoomId: 249866374
-        }
-      }
+      //   code: {},
+      code: "2TYLX",
+      //   session: {
+      //     "07mdu": {
+      //       code: "07mdu",
+      //       gameID: "",
+      //       playerIds: [2134, 1234, 1234],
+      //       zoomIds: [1234, 1234, 1234]
+      //     }
+      //   },
+      //   players: [
+      //     {
+      //       dummyid: {
+      //         character_id: "adffdcx",
+      //         chatroom_id: 1234,
+      //         user_uid: "B4Yo8z9ppGcNu4vNQYQOjkvLEz53",
+      //         vote_id: ""
+      //       }
+      //     }
+      //   ],
+      chatrooms: {},
+      players: []
+      //   chatrooms: {
+      //     adsfadfdv: {
+      //       id: "Living room",
+      //       players: ["Sepp", "Hansi"],
+      //       zoomId: 842749965
+      //     },
+      //     dfsdfga: {
+      //       id: "Kitchen",
+      //       players: ["Michi", "Anabel"],
+      //       zoomId: 598515719
+      //     },
+      //     fhjkfj: {
+      //       id: "Rooftop",
+      //       players: [],
+      //       zoomId: 249866374
+      //     }
+      //   }
     };
   },
-  name: "GameMap",
-  //     firestore: {
-  //       //   TODO
-  //       session: db.collection("sessions"),
-  //       players: db.collection("players"),
-  //       game: db.collection("games")
-  //     },
+  computed: {
+    ...mapGetters({
+      selectedGame: "selectedGame",
+      user: "user",
+      sessionCode: "sessionCode"
+    }),
+    displayCode() {
+      return this.code;
+    }
+  },
+  firestore: {
+    chatrooms: db.collection("chatrooms"),
+    sessions: db.collection("sessions")
+    // game: db.collection("games")
+  },
   methods: {
-    showGame() {
-      console.log("chatrooms", this.chatrooms);
+    logData() {},
+    putAllPlayersInFirstRoom() {
+      db.collection("sessions")
+        .doc(this.code)
+        .get()
+        .then(snapshot => {
+          const session = snapshot.data();
+          var allPlayers = session.playerIds;
+          db.collection("chatrooms")
+            .doc(this.chatrooms[0].id)
+            .update({ players: allPlayers });
+        });
+        // TODO remove all players from the other chatrooms
+    },
+
+    enterChatroom(room_id) {
+      // delete the player from the old chatroom, therefore iterate over all chatrooms
+      const email_to_switch = this.user.data.email;
+      db.collection("chatrooms")
+        .get()
+        .then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            // console.log(doc.id, " => ", doc.data());
+            var currentPlayers = doc.data().players;
+            if (currentPlayers == null) {
+              currentPlayers = [];
+            }
+
+            var index = currentPlayers.indexOf(email_to_switch);
+            if (index !== -1) {
+              currentPlayers.splice(index, 1);
+              db.collection("chatrooms")
+                .doc(doc.id)
+                .update({ players: currentPlayers });
+            }
+            // add player to the new chatroom
+            if (room_id == doc.id) {
+              currentPlayers.push(email_to_switch);
+              db.collection("chatrooms")
+                .doc(doc.id)
+                .update({ players: currentPlayers });
+            }
+          });
+        });
     }
   },
   created() {
-    this.showGame();
+    // this.code = this.sessionCode;
+    this.logData();
+    this.putAllPlayersInFirstRoom();
   }
 };
 </script>
