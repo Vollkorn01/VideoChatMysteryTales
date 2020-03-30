052192ce09753e27010c302809f2ff458b4a4170
Alexander Pfyffer <alexander.pfyffer@gmail.com>
2020-03-30 14:56:14 +0200

    waiting room setting description

--- src/components/WaitingRoom.vue
+++ src/components/WaitingRoom.vue
@@ -4,8 +4,17 @@
     <div>Code:</div>
     <div>{{sessionCode}}</div>
     <div>Send this code to your friends, so they can join!</div>
-      <v-btn @click="testGame">Refresh Player List</v-btn>
+    <h1>The Setting</h1>
+    <p>
+        The Phelps Case 2040 is set in the year 2040 in the Phelps Video Chat Group. The Phelps Chat Group was set up by the programmer Seth Phelps who has been mysteriously killed yesterday afternoon. Seth Phelps hacked the bank account of a big hedge fund, and there are multiple relatives who may benefit from his last will and testament distributing his fortune. Phelps’ lawyer has asked the family to gather together in the Phelps Chat Group, the day after the murder, to hear the reading of the will. The Police chat Bots are also present – because the Phelps family are the prime suspects in the murder case. Everyone listens attentively, as the Lawyer reads out the will. The main clause is very simple, as Phelps must have had a premonition or warning of his dreadful end. The whole of Phelps splendid fortune is left to whoever identifies his murderer! 
+    </p>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
 
+    <v-btn @click="refreshPlayerList">Refresh Player List</v-btn>
   </div>
 </template>
 <script>
@@ -19,28 +28,33 @@ export default {
     return {
       games: [],
       sessions: null,
+      playersInSession: [],
     };
   },
   firestore: {
     games: db.collection("games"),
-    sessions: db.collection("sessions"),
+    sessions: db.collection("sessions")
   },
   computed: {
     ...mapGetters({
       selectedGame: "selectedGame",
       user: "user",
       sessionCode: "sessionCode"
-    }),
+    })
   },
   methods: {
-      testGame() {
-          console.log('sessions', this.sessions)
-          console.log('sessionCode:', this.sessionCode)
-      }
+    refreshPlayerList() {
+      console.log("session:", this.sessions);
+      console.log("sessionCode:", this.sessionCode);
+      const result = this.sessions.filter(obj => {
+        return obj.id === this.sessionCode;
+      });
+      this.playerList = result.playerIds
+    }
   },
   created() {
-      this.code = this.sessionCode;
-  },
+    this.code = this.sessionCode;
+  }
 };
 </script>
 
