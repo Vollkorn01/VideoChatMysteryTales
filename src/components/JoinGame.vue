406cb8545057b9f6a0d6b21542d41b6b055b5fb5
Alexander Pfyffer <alexander.pfyffer@gmail.com>
2020-03-30 11:59:18 +0200

    user joins game vie sessionID

---
+++ src/components/JoinGame.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <v-text-field v-model="codeTextField" label="Code"></v-text-field>
+    <v-btn @click="joinSession()">Join Game</v-btn>
+  </div>
+</template>
+<script>
+// eslint-disable-next-line no-unused-vars
+import { db } from "../firebase";
+import { mapGetters } from "vuex";
+
+export default {
+  name: "JoinGame",
+  components: {},
+  data() {
+    return {
+      codeTextField: "",
+      label: "",
+      code: ""
+    };
+  },
+  computed: {
+    ...mapGetters({
+      selectedGame: "selectedGame",
+      user: "user",
+      sessionCode: "sessionCode"
+    })
+  },
+  methods: {
+    joinSession() {
+      // eslint-disable-next-line no-unused-vars
+      let joinSession = db
+        .collection("sessions")
+        .doc(this.codeTextField)
+        .get()
+        .then(snapshot => {
+          const session = snapshot.data();
+          if (!session) {
+            alert("Game Not Found");
+          } else {
+            this.setPlayer(session);
+          }
+          // do something with document
+          console.log(session);
+        });
+    },
+    setPlayer(session) {
+      session.playerIds.push(this.user.data.email);
+      // eslint-disable-next-line no-unused-vars
+      let createSession = db
+        .collection("sessions")
+        .doc(this.codeTextField)
+        .set(session);
+    }
+  },
+  created() {}
+};
+</script>
+
+<style>
+</style>
